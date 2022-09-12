763391d15913564c35ddb5a1003ac818f72f6bfc
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-09-12 11:04:41 -0700

    Add files via upload

---
+++ lucky.py
@@ -0,0 +1,35 @@
+import random
+num = []
+
+
+x = 1
+while x != 6:
+    guess = input('Pick a number 1-70')
+    while guess.isdigit() == False:
+        guess = input('no letters only numbers')
+    int(guess)
+    num.append(guess)
+    print(num)
+    x += 1
+    if int(guess) > 70:
+        print('LESS THAN 70!')
+    print(num)
+
+    
+
+#computers random luckynumbers
+comList = []
+x = 1
+while x !=6:
+    value = random.randint(1, 10)
+    x += 1
+    comList.append(value)
+    print(comList)
+    comList = []
+    l = list(set(l))
+
+
+
+
+       
+   
