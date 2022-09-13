953a2c3b253a495bb45fe3eb3f39e4f4f4bd32cc
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-09-13 11:05:18 -0700

    Add files via upload

--- lucky.py
+++ lucky.py
@@ -1,4 +1,6 @@
+from audioop import add
 import random
+from secrets import choice
 num = []
 
 
@@ -7,29 +9,29 @@ while x != 6:
     guess = input('Pick a number 1-70')
     while guess.isdigit() == False:
         guess = input('no letters only numbers')
-    int(guess)
-    num.append(guess)
-    print(num)
+    int(guess)   
     x += 1
     if int(guess) > 70:
-        print('LESS THAN 70!')
+        guess = input('LESS THAN 70!')
+    num.append(guess)
     print(num)
-
     
 
 #computers random luckynumbers
 comList = []
 x = 1
-while x !=6:
-    value = random.randint(1, 10)
+value = ("yurr") 
+comList = set()
+while x !=6: 
+    value = random.randint(1, 70)   
+    comList.add (value)  
     x += 1
-    comList.append(value)
-    print(comList)
-    #comList = []
-    #l = list(set(l))
+print(comList)
+
+
+#point system
 
 
 
 
        
-   
