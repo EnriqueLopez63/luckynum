717fbc5069d1150da593fbdb48857bee6318af61
EnriqueLopez63 <111899612+EnriqueLopez63@users.noreply.github.com>
2022-09-15 10:46:38 -0700

    Add files via upload

--- lucky.py
+++ lucky.py
@@ -1,7 +1,7 @@
 from audioop import add
 import random
 from secrets import choice
-num = []
+num = set()
 
 
 x = 1
@@ -13,23 +13,37 @@ while x != 6:
     x += 1
     if int(guess) > 70:
         guess = input('LESS THAN 70!')
-    num.append(guess)
+    num.add(guess)
     print(num)
     
 
 #computers random luckynumbers
-comList = []
-x = 1
+
 value = ("yurr") 
 comList = set()
-while x !=6: 
+while len(comList) != 5: 
     value = random.randint(1, 70)   
     comList.add (value)  
-    x += 1
 print(comList)
 
 
 #point system
+score = len(comList.intersection(num))
+
+if score == 0:
+    print("you got them all wrong")
+elif score ==1:
+    print("you got one right")
+elif score ==2:
+    print("you got two right")
+elif score ==3:
+    print("you got three right")
+elif score ==4:
+    print("you got four right")
+elif score ==5:
+    print("you got five right")
+
+
 
 
 
